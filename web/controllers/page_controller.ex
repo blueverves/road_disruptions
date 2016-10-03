c5cc74a23f05f193a4c009fc97780310c768a6b2
blueverves <volker.rabe@gmail.com>
2016-10-03 10:31:25 +0100

    render raw data stream in UI

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule RoadDisruptions.PageController do
   use RoadDisruptions.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    disruptions = StreamingXmlParser.run |> Stream.map(&(&1)) |> Enum.take 20
+    IO.inspect disruptions
+    render conn, "index.html", disruptions: disruptions
   end
 end
