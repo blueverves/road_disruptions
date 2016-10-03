740c81909a3d2de0c963d05c9c1a48ce6dae7e57
blueverves <volker.rabe@gmail.com>
2016-10-03 10:44:58 +0100

    render datetime fields

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,6 +1,13 @@
 defmodule RoadDisruptions.PageView do
   use RoadDisruptions.Web, :view
 
+  def format_datetime(datetime) do
+    case Timex.format(datetime, "{RFC1123}") do
+      {:ok, fdatetime} -> fdatetime
+      _ -> ""
+    end
+  end
+
   def format_point(point) do
     Enum.join(point, ", ")
   end
